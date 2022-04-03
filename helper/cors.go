53531b3a19daefc30bbf8d3cd84fee7369ca8bba
zidnim5 <zidni.mujib5@gmail.com>
2022-04-03 14:42:47 +0700

    header preflight

--- helper/cors.go
+++ helper/cors.go
@@ -2,8 +2,13 @@ package helper
 
 import "net/http"
 
-func Cors(writer *http.ResponseWriter) {
+func Cors(writer *http.ResponseWriter, r *http.Request) {
 	(*writer).Header().Set("Access-Control-Allow-Origin", "*")
 	(*writer).Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT")
 	(*writer).Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, Authorization")
+
+	if r.Method == "OPTIONS" {
+		(*writer).Write([]byte("allowed"))
+		return
+	}
 }
